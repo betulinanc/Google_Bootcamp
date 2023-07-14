492bf7fec1a001d2332e343a0e459aad1e6801c9
türkan ruva can <turkancan2002@hotmail.com>
2023-07-14 18:44:02 +0300

    Update and rename RegisterPage.dart to SignupPage.dart

--- lib/page/SignupPage.dart
+++ lib/page/SignupPage.dart
@@ -1,16 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:HomePage/page/HomePage.dart';
-import 'package:HomePage/page/LoginPage.dart';
 
-class RegisterPage extends StatefulWidget {
+import 'LoginPage.dart';
+
+
+
+
+
+class SignupPage extends StatefulWidget {
   @override
-  _RegisterPageState createState() => _RegisterPageState();
+  _SignupPageState createState() => _SignupPageState();
 }
 
-class _RegisterPageState extends State<RegisterPage> {
+
+class _SignupPageState extends State<SignupPage> {
   bool _SozlesmeAccepted = false;
   bool _KayitOlButtonEnabled = false;
   bool ButonAccepted = false;
+
+  final formkey = GlobalKey<FormState>();
+  final firebaseAuth  = FirebaseAuth.instance;
+  late String username;
+  late String password;
+  late String email;
+
 
   void _onaylandi() {
     showDialog(
@@ -83,152 +96,203 @@ class _RegisterPageState extends State<RegisterPage> {
 
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.pets, color: Colors.pink),
-                Text(
-                  'PetGuardian',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.pink,
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.pets, color: Colors.pink),
+                  Text(
+                    'PetGuardian',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink,
+                    ),
+                  ),
+                  Icon(Icons.pets, color: Colors.pink)
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.pets, color: Colors.black),
+                  Text(
+                    'BİLGİLERİNİZİ GİRİNİZ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Icon(Icons.pets, color: Colors.black),
+                ],
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'E-posta boş bırakılamaz.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+
+                    email = value!;
+
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'E-posta',
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-                Icon(Icons.pets, color: Colors.pink)
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.pets, color: Colors.black),
-                Text(
-                  'BİLGİLERİNİZİ GİRİNİZ',
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Kullanıcı adı boş bırakılamaz.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    username=value!;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Kullanıcı Adı',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Şifre boş bırakılamaz.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+
+                    password = value!;
+
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Parola',
+
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed:_KayitOlButtonEnabled ?() async {
+                  if (formkey.currentState!.validate()){
+                    formkey.currentState!.save();
+                    try{
+                      var userKaydet = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password,);
+                      formkey.currentState!.reset();
+                      print(userKaydet.user!.uid);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                      print('Giriş yapıldı. Kullanıcı adı: $username');
+                    }catch(e){
+                      print('Kayıt işlemi gerçekleşemedi: $e');
+                    }
+
+
+
+                  }else{}
+
+
+                }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+                child: Text(
+                  'KAYIT OL',
                   style: TextStyle(
-                    fontSize: 16,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
+
+                  ),
+                ),
+              ),
+
+
+              SizedBox( width: 50),
+              Row(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Checkbox(
+                    value: _SozlesmeAccepted,
+                    onChanged: (value) {
+                      setState(() {
+                        _SozlesmeAccepted =  false;
+
+                      });
+                    },
+                  ),
+                  TextButton(
+                    onPressed: _onaylandi,
+
+                    child: Text('Sözleşmeyi okuyup, Kabul ediniz.'),
+                  ),
+                  SizedBox(height: 20),
+
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white70,
+                ),
+
+                child: Text(
+                  'Giriş Sayfasına Geri Dön',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
                     color: Colors.black,
                   ),
                 ),
-                Icon(Icons.pets, color: Colors.black),
-              ],
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'E-posta',
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Kullanıcı Adı',
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Parola',
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                obscureText: true,
-              ),
-            ),
-
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed:_KayitOlButtonEnabled ?() {
-
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              }
-                  : null,
-              style: ElevatedButton.styleFrom(
-                primary: Colors.black,
-              ),
-              child: Text(
-                'KAYIT OL',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-
-                ),
-              ),
-            ),
-            SizedBox( width: 50),
-            Row(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Checkbox(
-                  value: _SozlesmeAccepted,
-                  onChanged: (value) {
-                    setState(() {
-                      _SozlesmeAccepted =  false;
-
-                    });
-                  },
-                ),
-                TextButton(
-                  onPressed: _onaylandi,
-
-                  child: Text('Sözleşmeyi okuyup, Kabul ediniz.'),
-                ),
-                SizedBox(height: 20),
-
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white70,
-              ),
-
-              child: Text(
-                'Giriş Sayfasına Geri Dön',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
